901a7e8b6030f9db8a864395d1e822b824dec6e6
Alexandra <alexandra-sh@mai.ru>
2023-09-09 17:22:56 +0300

    задачи с третьего семинара

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+//и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+//вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+// по координатам точки оперделяем номер четверти
+int QterTest(int x, int y)
+{
+    if (x>0&&y>0) return 1;
+    if (x<0&&y>0) return 2;
+    if (x<0&&y<0) return 3;
+    if (x>0&&y<0) return 4;
+
+    return 0;
+}
+
+//печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int x = ReadData("Введите координату Х:");
+int y = ReadData("Введите координату Y:");
+
+int res = QterTest(x,y);
+
+PrintResult("Точка находится в четверти № "+res);
+
---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает
+// диапазон возможных координат точек в этой четверти (x и y).
+
+
+//вводим данные
+int ReadData(int num)
+{
+    Console.WriteLine(num);
+    int numQuarter1 = int.Parse(Console.ReadLine()??"0");
+    return numQuarter1;
+}
+// по координатам точки оперделяем номер четверти
+string QuarterBorderAsk(int numQuarter)
+{
+    if (numQuarter == 1) return "x>0, y>0";
+    if (numQuarter == 2) return "x<0, y>0";
+    if (numQuarter == 3) return "x<0, y<0";
+    if (numQuarter == 4) return "x>0, y<0";
+
+    return String.Empty;
+}
+
+//печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+string x = ReadData("Введите номер четверти:");
+
+int res = QuarterBorderAsk(x);
+
+PrintResult("Точка находится в четверти № "+res);
+
