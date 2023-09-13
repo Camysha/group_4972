5441922411570ab4d4520dfa6500f1081d064cb2
Alexandra <alexandra-sh@mai.ru>
2023-09-13 05:02:59 +0300

    исправлена 8 задача

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,13 +1,22 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), 
 // а на выходе показывает все четные числа в промежутке от 1 до N.
 
-Console.WriteLine("Enter number");
-int numN = int.Parse(Console.ReadLine()??"0");
-int evNumN = 2;
+Console.Write("Введите число: "); // Создаем строку ввода
+string? inputlineN = Console.ReadLine(); // Инициализируем сканер
+int startNumber = 2; // Переменна, с кот начинается отчет
 
-while (evNumN<numN)
+if (inputlineN != null) // Проверяем, что строка не пустая
 {
-	Console.Write (evNumN+",");
-   evNumN += 2;
+int inputnumberN = int.Parse(inputlineN); // Парсим строку в инт
+while (startNumber <= inputnumberN - 2) // Условие завершения цикла
+{
+Console.Write(startNumber + ", "); // Если число делится - выводим в консоль
+startNumber = startNumber + 2; // прибавляем единицу к числу
+}
+
+if (startNumber % 2 == 0)
+{
+Console.WriteLine(startNumber);
+}
 }
   
