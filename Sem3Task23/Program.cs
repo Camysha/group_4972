af2c479aced0a9541f80c01222bdf1487f08f7e2
Alexandra <alexandra-sh@mai.ru>
2023-09-15 02:20:58 +0300

    добавлено дз к 3 семинару

---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Answer
+{
+   static void ShowCube(int N)
+    {
+      // Введите свое решение ниже
+       int i = 1;
+       int CubeRes = 0;
+        while (i<= N)
+        {
+            CubeRes = (int)Math.Pow(i, 3);
+            Console.WriteLine(CubeRes);
+            i++;
+        }
+    }
+   
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 5;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
